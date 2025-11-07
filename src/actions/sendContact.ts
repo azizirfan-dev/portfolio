62427006bcacee41040845746450d3343f8bd52d
azizirfan-dev <azizirfan60@gmail.com>
2025-11-07 21:39:03 +0700

    fix: backendless env and axios baseURL

--- src/actions/sendContact.ts
+++ src/actions/sendContact.ts
@@ -1,14 +1,21 @@
+"use server";
 import { backendless } from "@/lib/axios";
 
-export interface ContactData { 
-    name:string; 
-    email:string; 
-    subject: string; 
-    message:string;
+export interface ContactData {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
 }
 
 export async function sendContact(data: ContactData) {
-    const table = process.env.NEXT_PUBLIC_BACKENDLESS_TABLE || "Contact";
-    const res = await backendless.post(`/data/${table}`, data);
-    return(res.data)
-};
+  const TABLE = process.env.NEXT_PUBLIC_BACKENDLESS_TABLE || "Contact";
+
+  try {
+    const res = await backendless.post(`data/${TABLE}`, data);
+    return res.data;
+  } catch (err: any) {
+    console.error("Backendless Error:", err.response?.data || err.message);
+    throw new Error("Gagal mengirim pesan ke server.");
+  }
+}
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,8 +4,8 @@ const APP_ID = process.env.NEXT_PUBLIC_BACKENDLESS_APP_ID!;
 const API_KEY = process.env.NEXT_PUBLIC_BACKENDLESS_API_KEY!;
 
 export const backendless = axios.create ({ 
-    baseURL: `https://api.backendless.com/${APP_ID}/${API_KEY}`,
+    baseURL: `https://api.backendless.com/${APP_ID}/${API_KEY}/`,
     headers: { 
-        contentType: "application/json",
+        "Content-Type": "application/json",
     },
 });
